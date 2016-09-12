c2c36747ef997a20709c1bb2b2464570de0cf240
Tram <tramngo2@gmail.com>
2016-09-11 22:36:23 -1000

    finished

--- context.js
+++ context.js
@@ -17,14 +17,14 @@ var wall_e = new Robot("Wall-E");
 
 
 // #1
-console.log(  );
+console.log(add(0,1));
 
 // #2  
-console.log(  );
+console.log(add.call(voltron,2,3));
 
 // #3
-console.log(  );
+console.log(add.apply(optimus,20,30));
 
 // #4
-
-console.log(  );
+var calculate = add.bind(bender);
+console.log(calculate('drinking', 'beer'));
